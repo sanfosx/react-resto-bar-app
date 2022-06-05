f539009467f8c1459035a6f0eff25e2dd8f9be96
sanfosx <sanfosx@hotmail.comm>
2022-06-05 04:54:54 -0300

    Configuracion inicial.. carpetas librerias: bootstrap, bootswacht, firebase, materialize icons, styled components, react router dom

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>app</h1>
+      <i className='material-icons text-info'>create</i>
+      <i className='material-icons text-danger'>close</i>
+      
+    </div>
+  );
+}
+
+export default App;
